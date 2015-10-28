c3f187143229c47608e75f14f002267781c0caef
Adam Ki Jensen <adajensen@gmail.com>
2015-10-27 19:01:35 -0600

    big refactor. adding a dahsboard controller as it seems to be a better design decision

--- app/services/github.rb
+++ app/services/github.rb
@@ -1,15 +1,23 @@
 class Github
-  attr_reader :user, :connection
+  attr_reader :connection, :user
 
   def initialize(user)
     @user = user
-    @connection = Hurley.new("http://api.github.com")
-    @connection.query[:access_token] = user.oauth_token
+    @connection = Hurley::Client.new("https://api.github.com")
+    connection.query[:access_token] = user.oauth_token
+  end
+
+  def find_user_followers(user)
+    parse(connection.get("users/#{user.nickname}/followers"))
+  end
+
+  def find_user_repos(user)
+    parse(connection.get("users/#{user.nickname}/repos"))
   end
 
   private
 
     def parse(response)
-      JSON.parse(response).body
+      JSON.parse(response.body, symbolize_names: true)
     end
 end
