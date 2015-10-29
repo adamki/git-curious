c8f1001cb5ae7fea8e45954adf16ccb5e4822574
Adam Ki Jensen <adajensen@gmail.com>
2015-10-28 21:21:27 -0600

    tests are now passing for GithubService Model

--- test/services/github_service_test.rb
+++ test/services/github_service_test.rb
@@ -4,25 +4,36 @@ class GithubServiceTest < ActiveSupport::TestCase
   attr_reader :service, :user
 
   def setup
-    @user = User.create!(
-         id: 1,
+    @user ||= User.find_or_create_by(
          nickname: "adamki",
          uid: "10334352",
-         oauth_token: ENV["CLIENT_ID"],
+         oauth_token: ENV["USER_TOKEN"],
          email: "adajensen@gmail.com",
          image_url: "https://avatars.githubusercontent.com/u/10334352?v=3",
-         followers: "4",
-         following: "19"
     )
     @service ||= GithubService.new(user)
   end
 
-  test "#find_user_followers" do
-    VCR.use_cassette('github#find_user_followers_test') do
+  test "#find_user_repos" do
+    VCR.use_cassette('github_service#find_user_repos_test') do
+      repos = service.find_user_repos(user)
+      repo  = repos.first
+      assert_equal 30, repos.count
+    end
+  end
+
+  test "find_user_followers_count" do
+    VCR.use_cassette('github_service#find_user_followers_test') do
       followers = service.find_user_followers(user)
       follower  = followers.first
-
       assert_equal 4, followers.count
+    end
+  end
+
+  test 'find_users_followees_test' do
+    VCR.use_cassette('github_service#find_user_following') do
+      following = service.find_user_following(user)
+      assert_equal 19, following.count
     end
   end
 end
